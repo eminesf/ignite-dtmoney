f1b53c0f35cc1a949ab66c0dbcdfbb5c1d1ca2e9
Emiliano <emilianofuchs@gmail.com>
2021-11-17 17:59:48 -0300

    adicionando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [newTransactionModal, setNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModal(false);
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={newTransactionModal}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionTable() {
+  useEffect(() => {
+    api.get("/transactions").then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -20,7 +26,7 @@ export function TransactionTable() {
             <td>16/11/2021</td>
           </tr>
           <tr>
-            <td>Aliguel</td>
+            <td>Aluguel</td>
             <td className="withdraw">- R$ 1.100</td>
             <td>Casa</td>
             <td>17/10/2021</td>
